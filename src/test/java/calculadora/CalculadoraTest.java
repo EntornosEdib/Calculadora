405e7cd8b5ea4d1f7d445531bc08f74cd43ae7f1
Rafael Cuello <rafaelcuello@docente.edib.es>
2020-04-25 15:18:08 +0200

    Prueba Unitaria JUnit - Fail

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -1,23 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculadora;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
-/**
- *
- * @author Rafael
- */
 public class CalculadoraTest {
-
-    /**
-     *
-     */
    @Test
     public void testSuma(){
          int resultado=Calculadora.suma(2,3);
