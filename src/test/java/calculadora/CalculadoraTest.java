7af943e9a8d1f36b9311e80106532a26f45bdaa1
Rafael Cuello <rafaelcuello@docente.edib.es>
2020-04-25 14:55:12 +0200

    Prueba Unitaria JUnit - OK

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -18,9 +18,17 @@ public class CalculadoraTest {
     /**
      *
      */
+   @Test
+    public void testSuma(){
+         int resultado=Calculadora.suma(2,3);
+         int esperado=5; // 2 + 3 = 5
+         assertEquals(esperado,resultado);
+    } 
     @Test
-    public void CalculadoraTest() {
-        fail("Función no implementada aún");
-    }
-    
+    public void testResta(){
+         int resultado=Calculadora.resta(3,2);
+         int esperado=1; // 3 - 2 = 1
+         assertEquals(esperado,resultado);
+    } 
 }
+    
