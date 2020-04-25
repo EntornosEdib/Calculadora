b8c8feb91fa16f38ccea902172649e23f872c776
Rafael Cuello <rafaelcuello@docente.edib.es>
2020-04-25 12:51:13 +0200

    Prueba Unitaria Manual - OK

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -10,6 +10,16 @@ package calculadora;
  * @author Rafael
  */
 public class Calculadora {
+public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_BLACK = "\u001B[30m";
+public static final String ANSI_RED = "\u001B[31m";
+public static final String ANSI_GREEN = "\u001B[32m";
+public static final String ANSI_YELLOW = "\u001B[33m";
+public static final String ANSI_BLUE = "\u001B[34m";
+public static final String ANSI_PURPLE = "\u001B[35m";
+public static final String ANSI_CYAN = "\u001B[36m";
+public static final String ANSI_WHITE = "\u001B[37m";
+public static final String newLine = System.getProperty("line.separator");
     public static int suma(int a, int b){
         return a+b;
     }
@@ -26,6 +36,14 @@ public class Calculadora {
         int a=3;
         int b=2;
         int res = suma(a,b);
-        System.out.println("3 + 2 = 5");
+        int esperado=5;
+        if (res == esperado){
+            System.out.print(ANSI_GREEN+"Este programa está bien hecho"+ANSI_RESET+newLine);
+            System.out.println("3 + 2 = 5");
+        } else {
+            System.out.print(ANSI_RED+"Este programa está mal hecho"+ANSI_RESET+newLine);
+            System.out.println("3 + 2 NO ES 5");
+        }
+        
     }
 }
