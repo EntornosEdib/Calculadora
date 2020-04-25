384b9a1faef3edf135263ee2865fd543c40fb38b
Rafael Cuello <rafaelcuello@docente.edib.es>
2020-04-25 11:15:46 +0200

    Version 1.0 - Inicial

---
+++ src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Calculadora {
+    public static int suma(int a, int b){
+        return a+b;
+    }
+    public static int resta(int a, int b){
+        return a-b;
+    }
+    public static int multiplica(int a, int b){
+        return a*b;
+    }
+    public static int divide(int a, int b){
+        return a/b;
+    }
+    public static void main(String args[]){
+        int a=3;
+        int b=2;
+        int res = suma(a,b);
+        System.out.println("3 + 2 = 5");
+    }
+}
