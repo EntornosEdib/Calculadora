4cf5a0958326cb378a4c6787180272baaa9eb193
Rafael <Rafael@10.0.3.5>
2021-05-24 11:05:04 +0200

    modificacion rama tperez desde netbeans

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -6,13 +6,13 @@
 package calculadora;
 
 //junit 4
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 //junit 5
-import org.junit.Test;
+//import org.junit.Test;
 //junit 4
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 //junit 5
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 
 /**
